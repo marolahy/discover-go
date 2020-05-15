149fd9ed3ef76ac1727f76d0f33d3d9e87a42826
Randriamifidy <bezama.randriamifidy@cgi.com>
2020-05-14 22:05:15 -0400

    [initial] creation premier package

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/marolahy/discover-go/models"
+)
 
 func main() {
-	fmt.Println("Hello from Go")
+	u := models.User{
+		ID:        2,
+		FirstName: "Beza",
+		LastName:  "RAND",
+	}
+	fmt.Println(u)
 }
